7561218fbf3ce4e86977bcb1efd948a8ea19bc70
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-07-21 08:09:54 +0900

    :lipstick: Footer에 write 버튼으로 수정
    footer에 write 버튼으로 수정

--- src/libs/components/footer.tsx
+++ src/libs/components/footer.tsx
@@ -1,19 +1,31 @@
+import Link from "next/link";
 import { AiOutlineHome, AiOutlineSearch } from "react-icons/ai";
-import { MdOutlineLocalPostOffice } from "react-icons/md";
+import { FiFeather } from "react-icons/fi";
 import { HiOutlineUser } from "react-icons/hi";
+
 export default function footer() {
   return (
     <footer className="border-t bg-white sticky bottom-0 border-[#eff3f4]">
       <div className="grid grid-cols-4 h-14">
-        <div className="flex items-center justify-center">
-          <AiOutlineHome size="24px" className="cursor-pointer" />
-        </div>
+        <Link
+          href="/"
+          className="flex items-center justify-center cursor-pointer"
+        >
+          <div>
+            <AiOutlineHome size="24px" />
+          </div>
+        </Link>
         <div className="flex items-center justify-center">
           <AiOutlineSearch size="24px" className="cursor-pointer" />
         </div>
-        <div className="flex items-center justify-center">
-          <MdOutlineLocalPostOffice size="24px" className="cursor-pointer" />
-        </div>
+        <Link
+          href="/write"
+          className="flex items-center justify-center cursor-pointer"
+        >
+          <div>
+            <FiFeather size="24px" />
+          </div>
+        </Link>
         <div className="flex items-center justify-center">
           <HiOutlineUser size="24px" className="cursor-pointer" />
         </div>
