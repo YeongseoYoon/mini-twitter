1372982c1b29380e0147e92c0c440c4769d8c024
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-07-21 15:33:23 +0900

    :recycle: 랜덤 16진수 색상 추출 함수 수정

--- src/libs/utils/getRandomHexColor.ts
+++ src/libs/utils/getRandomHexColor.ts
@@ -1,6 +1,6 @@
 export const getRandomHexColor = () => {
   const letters = "0123456789ABCDEF";
-  let color = "#";
+  let color = "";
   for (let i = 0; i < 3; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
