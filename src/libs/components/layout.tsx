5231cd219f8f05e740f904162239c9e25a47b170
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-07-21 09:14:28 +0900

    :lipstick: Layout 스타일 수정
    layout 스타일 수정

--- src/libs/components/layout.tsx
+++ src/libs/components/layout.tsx
@@ -3,7 +3,7 @@ import Header from "./header";
 
 export default function Layout({ children }: React.PropsWithChildren) {
   return (
-    <div className="flex justify-center min-w-[576px] max-w-5xl w-full">
+    <div className="flex justify-center min-w-[576px] max-w-5xl w-full m-auto">
       <div className="flex flex-col justify-center">
         <Header />
         <main className="flex flex-col min-w-[576px] items-center flex-1">
