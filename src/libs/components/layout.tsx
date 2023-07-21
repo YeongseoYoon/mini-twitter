fe035b0891576df1cd144c14fb4ad87ceb8fd0ef
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-07-21 15:55:49 +0900

    :lipstick: 화면 하단에 footer 고정
    화면 하단에 footer 고정

--- src/libs/components/layout.tsx
+++ src/libs/components/layout.tsx
@@ -3,10 +3,10 @@ import Header from "./header";
 
 export default function Layout({ children }: React.PropsWithChildren) {
   return (
-    <div className="flex justify-center min-w-[576px] max-w-5xl w-full m-auto">
+    <div className="flex justify-center min-w-[576px] max-w-5xl min-h-screen w-full m-auto">
       <div className="flex flex-col justify-center">
         <Header />
-        <main className="flex flex-col min-w-[576px] items-center flex-1">
+        <main className="flex flex-col min-w-[576px] flex-grow items-center flex-1">
           {children}
         </main>
         <Footer />
