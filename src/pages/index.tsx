591def869263d3cf24de57a8c31a71f5ff75f362
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-11-09 15:50:06 +0900

    ♻️ refactor: home 페이지에 ssr 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
+import { NextPage } from "next";
 import Head from "next/head";
-import useSWR from "swr";
+import useSWR, { SWRConfig } from "swr";
 import { Tweet } from "@prisma/client";
 import { Layout, TweetCard } from "@/libs/components";
 import { UserWithoutPassword } from "@/types";
-
+import { client } from "@/libs/server";
 interface Tweets extends Tweet {
   user: UserWithoutPassword;
   _count: {
@@ -38,4 +39,48 @@ const Home = () => {
   );
 };
 
-export default Home;
+const Page: NextPage<{ tweets: Tweets[] }> = ({ tweets }) => {
+  return (
+    <SWRConfig
+      value={{
+        fallback: {
+          "/api/tweets": {
+            isSuccess: true,
+            tweets,
+          },
+        },
+      }}
+    >
+      <Home />
+    </SWRConfig>
+  );
+};
+
+export async function getServerSideProps() {
+  const tweets = await client.tweet.findMany({
+    include: {
+      _count: {
+        select: {
+          favorites: true,
+        },
+      },
+      user: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+          email: true,
+          createdAt: true,
+        },
+      },
+    },
+  });
+
+  return {
+    props: {
+      tweets: JSON.parse(JSON.stringify(tweets)),
+    },
+  };
+}
+
+export default Page;
